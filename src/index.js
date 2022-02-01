88ca968698d5840dbc4cabe47ee02530781be6da
Liam Eller <liamthelastson@gmail.com>
2022-02-01 09:23:14 -0800

    correctly excport & import dependencies
    Co-Authored-By: Zachary Green <ultrazacky@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,78 +1,8 @@
-function Game(runningScore) {
-  this.runningScore = runningScore;
-  this.currentId = 0;
-  this.players = {};
-  this.playerTurnBoolean = 1;
-}
-
-function die() {
-  return Math.floor((Math.random()*6)+1);
-}
-
-Game.prototype.playerTurn = function(currentRoll) {
-  if (currentRoll === 1) {
-    gameBoard.runningScore = 0;
-    gameBoard.endTurn();
-  }
-  else {
-    gameBoard.runningScore += currentRoll;
-  }
-}
-
-Game.prototype.addPlayer = function(player) {
-  player.id = this.assignId();
-  this.players[player.id] = player;
-}
-
-Game.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId;
-}
-
-Game.prototype.endTurn = function() {
-  if (this.playerTurnBoolean === true) {
-    this.players[1].score += this.runningScore;
-    this.playerTurnBoolean = false;
-    this.winner();
-    this.runningScore = 0;
-  } else {
-    this.players[2].score += this.runningScore;
-    this.playerTurnBoolean = true;
-    this.winner();
-    this.runningScore = 0;
-  }
-}
-
-// Game.prototype.endTurn = function() {
-//   for (let i = 1; i < this.currentId; i++) {
-//     this.players[i].score += this.runningScore;
-//     this.winner();
-//     this.runningScore = 0;
-//   }
-// }
-
-Game.prototype.winner = function() {
-  Object.keys(gameBoard.players).forEach(function(key) {
-    console.log("key: " + key + "gameboard: " + gameBoard.players[key])
-    if (gameBoard.players[parseInt(key)].score >= 100) {
-      alert("player " + gameBoard.players[parseInt(key)].name + "wins with a score of " + gameBoard.players[parseInt(key)].score + " !!");
-    }
-  })
-}
-
-function Player(score, name) {
-  this.score = score;
-  this.name = name;
-}
-
-function getPlayerTurn() {
-  // die()
-  if (gameBoard.playerTurnBoolean === true) {
-    return "Player1"
-  } else {
-    return "player2"
-  }
-}
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './css/index.css';
+import { Game, Player, getPlayerTurn, die} from "./js/business.js";
 
 let gameBoard = new Game(0,0)
 let player1 = new Player(0, "liam")
---
+++ src/js/business.js
@@ -0,0 +1,75 @@
+export function Game(runningScore) {
+  this.runningScore = runningScore;
+  this.currentId = 0;
+  this.players = {};
+  this.playerTurnBoolean = 1;
+}
+
+export function die() {
+  return Math.floor((Math.random()*6)+1);
+}
+
+Game.prototype.playerTurn = function(currentRoll) {
+  if (currentRoll === 1) {
+    this.runningScore = 0;
+    this.endTurn();
+  }
+  else {
+    this.runningScore += currentRoll;
+  }
+};
+
+Game.prototype.addPlayer = function(player) {
+  player.id = this.assignId();
+  this.players[player.id] = player;
+};
+
+Game.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+Game.prototype.endTurn = function() {
+  if (this.playerTurnBoolean === true) {
+    this.players[1].score += this.runningScore;
+    this.playerTurnBoolean = false;
+    this.winner();
+    this.runningScore = 0;
+  } else {
+    this.players[2].score += this.runningScore;
+    this.playerTurnBoolean = true;
+    this.winner();
+    this.runningScore = 0;
+  }
+};
+
+// Game.prototype.endTurn = function() {
+//   for (let i = 1; i < this.currentId; i++) {
+//     this.players[i].score += this.runningScore;
+//     this.winner();
+//     this.runningScore = 0;
+//   }
+// }
+
+Game.prototype.winner = function() {
+  Object.keys(this.players).forEach(function(key) {
+    console.log("key: " + key + "gameboard: " + this.players[key])
+    if (this.players[parseInt(key)].score >= 100) {
+      alert("player " + this.players[parseInt(key)].name + "wins with a score of " + this.players[parseInt(key)].score + " !!");
+    }
+  });
+};
+
+export function Player(score, name) {
+  this.score = score;
+  this.name = name;
+}
+
+export function getPlayerTurn() {
+  // die()
+  if (this.playerTurnBoolean === true) {
+    return "Player1";
+  } else {
+    return "player2";
+  }
+}
